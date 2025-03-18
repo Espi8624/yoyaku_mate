a8966da9733ddfb49266c3ca6b56989ea8e75836
spy <espi8624@gmail.com>
2025-03-18 23:16:58 +0900

    Add Frequent Places area & Update Navbar

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,9 +1,33 @@
 import './MainPage.css';
 
+const frequentPlaces = [
+    "Central park",
+    "Times Square",
+    "Empire State Building",
+    "Statue of Liberty",
+    "Brooklyn Bridge",
+];
+
 function MainPage() {
-    return(
+    return (
         <div className="main-page">
-            <h1>MainPage</h1>
+            <div className='main-calendar'></div>
+
+            <div className='main-frequent-places'>
+                <div className='main-frequent-places-wrap'>
+                    <h1 className='title'>Frequent Places</h1>
+                    <ul>
+                        {frequentPlaces.map((place, index) => (
+                            <li key={index}>
+                                {place}
+                                <icon className="move-icon"> &gt;</icon>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+            <div className='main-time-line'></div>
         </div>
     );
 }
