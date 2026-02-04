da598574c68d77754f90a8de5188df08d6391bc7
spy <espi8624@gmail.com>
2026-02-04 22:37:50 +0900

    メニュー多国語化

--- src/containers/waiting-screen/waiting-screen-menu/WaitingScreenMenu.jsx
+++ src/containers/waiting-screen/waiting-screen-menu/WaitingScreenMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useWaitingScreen } from "../WaitingScreenContext";
 import useTranslation from "../../../hook/useTranslation";
+import { getTranslatedText } from "../../../utils/i18nHelper";
 import { getMenuList } from "../../../api/waitingService";
 import CommonPopup from "../../../components/CommonPopup";
 import BackButton from "../../../components/BackButton";
@@ -103,6 +104,7 @@ function WaitingScreenMenu() {
                         {
                             menuId: menu.menu_id,
                             name: menu.title,
+                            title_translations: menu.title_translations, // Add translations
                             price: menu.price,
                             quantity: newQuantity,
                             imageUrl: menu.menu_image_url,
@@ -145,65 +147,75 @@ function WaitingScreenMenu() {
             ) : (
                 <div className="menu-selection-container">
                     <div className="menu-list">
-                        {menuList.map((menu) => (
-                            <div
-                                key={menu.menu_id}
-                                className="menu-item-card"
-                            >
-                                <div className="menu-item-top-row">
-                                    {menu.menu_image_url ? (
-                                        <img
-                                            src={menu.menu_image_url}
-                                            alt={menu.title}
-                                            className="waiting-menu-item-image"
-                                        />
-                                    ) : (
-                                        <div className="menu-item-placeholder">No Image</div>
-                                    )}
-                                    <div className="menu-item-details">
-                                        <div className="menu-item-top-row">
-                                            <div className="menu-item-info">
-                                                <div className="menu-item-title">{menu.title}</div>
-                                                <div className="menu-item-price">¥{menu.price.toFixed(1)}</div>
-                                                {menu.description && (
+                        {menuList.map((menu) => {
+                            const displayTitle = getTranslatedText(menu.title, menu.title_translations, selectedLanguageCode);
+                            const displayDescription = getTranslatedText(menu.description, menu.description_translations, selectedLanguageCode);
+
+                            const titleParts = displayTitle.split(" / ");
+                            const mainTitle = titleParts[0];
+                            const pronunciation = titleParts.length > 1 ? titleParts[1] : null;
+
+                            return (
+                                <div
+                                    key={menu.menu_id}
+                                    className="menu-item-card"
+                                >
+                                    <div className="menu-item-top-row">
+                                        {menu.menu_image_url ? (
+                                            <img
+                                                src={menu.menu_image_url}
+                                                alt={mainTitle}
+                                                className="waiting-menu-item-image"
+                                            />
+                                        ) : (
+                                            <div className="menu-item-placeholder">No Image</div>
+                                        )}
+                                        <div className="menu-item-details">
+                                            <div className="menu-item-title-container">
+                                                <div className="menu-item-title">{mainTitle}</div>
+                                                {pronunciation && <div className="menu-item-pronunciation">{pronunciation}</div>}
+                                            </div>
+                                            <div className="menu-item-price-row">
+                                                <div className="menu-item-price">¥{Number(menu.price).toLocaleString()}</div>
+                                                <div className="menu-item-controls">
                                                     <button
-                                                        onClick={() => toggleDescription(menu.menu_id)}
-                                                        className="menu-item-toggle-btn"
+                                                        className="quantity-btn minus"
+                                                        onClick={() => handleQuantityChange(menu, -1)}
+                                                        disabled={getQuantity(menu.menu_id) === 0}
                                                     >
-                                                        {expandedMenus.has(menu.menu_id) ? `▲ ${menuText.close_details}` : `▼ ${menuText.view_details}`}
+                                                        <svg width="12" height="2" viewBox="0 0 12 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <rect width="12" height="2" rx="1" fill="#333" />
+                                                        </svg>
                                                     </button>
-                                                )}
+                                                    <span className="quantity-value">{getQuantity(menu.menu_id)}</span>
+                                                    <button
+                                                        className="quantity-btn plus"
+                                                        onClick={() => handleQuantityChange(menu, 1)}
+                                                    >
+                                                        <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path fillRule="evenodd" clipRule="evenodd" d="M7 5V0H5V5H0V7H5V12H7V7H12V5H7Z" fill="#333" />
+                                                        </svg>
+                                                    </button>
+                                                </div>
                                             </div>
-                                            <div className="menu-item-controls">
+                                            {displayDescription && (
                                                 <button
-                                                    className="quantity-btn minus"
-                                                    onClick={() => handleQuantityChange(menu, -1)}
-                                                    disabled={getQuantity(menu.menu_id) === 0}
+                                                    onClick={() => toggleDescription(menu.menu_id)}
+                                                    className="menu-item-toggle-btn"
                                                 >
-                                                    <svg width="12" height="2" viewBox="0 0 12 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <rect width="12" height="2" rx="1" fill="#333" />
-                                                    </svg>
+                                                    {expandedMenus.has(menu.menu_id) ? `▲ ${menuText.close_details}` : `▼ ${menuText.view_details}`}
                                                 </button>
-                                                <span className="quantity-value">{getQuantity(menu.menu_id)}</span>
-                                                <button
-                                                    className="quantity-btn plus"
-                                                    onClick={() => handleQuantityChange(menu, 1)}
-                                                >
-                                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path fillRule="evenodd" clipRule="evenodd" d="M7 5V0H5V5H0V7H5V12H7V7H12V5H7Z" fill="#333" />
-                                                    </svg>
-                                                </button>
-                                            </div>
+                                            )}
                                         </div>
                                     </div>
+                                    {expandedMenus.has(menu.menu_id) && displayDescription && (
+                                        <div className="menu-item-description-box">
+                                            {displayDescription}
+                                        </div>
+                                    )}
                                 </div>
-                                {expandedMenus.has(menu.menu_id) && menu.description && (
-                                    <div className="menu-item-description-box">
-                                        {menu.description}
-                                    </div>
-                                )}
-                            </div>
-                        ))}
+                            );
+                        })}
                     </div>
                 </div>
             )}
